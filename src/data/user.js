e4ffea019d851f057182946d19b12e4309bd89b0
Hassan3322751 <141483001+Hassan3322751@users.noreply.github.com>
2024-09-19 08:30:13 -0700

    Hassan Dev

---
+++ src/data/user.js
@@ -0,0 +1,99 @@
+const INFO = {
+	main: {
+		title: "Hassan-Dev Portfolio",
+		name: "Hassan Raza",
+		email: "mianhassan155156@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		github: "https://github.com/Hassan3322751",
+		linkedin: "https://www.linkedin.com/in/hassan-raza-a05500285/",
+		instagram: "https://www.instagram.com/mianhassan6964/",
+	},
+
+	homepage: {
+		title: "Full-stack web developer, and api developer.",
+		description:
+			"I am a full stack web developer with expertise in React.js Express.js Node.js MongoDB. I have experience in building scalable, secure and reliable web applications using popular framework and technologies. ",
+	},
+
+	about: {
+		title: "I’m Hassan Raza. I live in Pakistan, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out and suggest any improvements or enhancements you might have in mind.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://hassan3322751.github.io/react-cart/",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://hassan3322751.github.io/gradiant/",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://store-managment.vercel.app/",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://hassan3322751.github.io/Age-Calculator/",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://hassan3322751.github.io/Pass-Generator/",
+		},
+
+		{
+			title: "Project 6",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://hassan3322751.github.io/Google-Keep/",
+		},
+
+		{
+			title: "Project 7",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://social-boost-six.vercel.app/",
+		},
+	],
+};
+
+export default INFO;
