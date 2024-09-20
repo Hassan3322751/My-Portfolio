b38cd826fe2cb93a4548e4ee537204f03b0989af
Hassan3322751 <141483001+Hassan3322751@users.noreply.github.com>
2024-09-19 20:58:45 -0700

    Main Url Updated

--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -19,7 +19,7 @@ const NavBar = (props) => {
 										: "nav-item"
 								}
 							>
-								<Link to="/">Home</Link>
+								<Link to="/My-Portfolio">Home</Link>
 							</li>
 							<li
 								className={
